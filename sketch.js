b2bba89bbb05e96bb099297c957ae47de4250f0d
Sarah Mizrahi <sarah.mizrahi@mdycehs.net>
2017-03-23 18:33:56 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+    createCanvas(800,800);
+}
+
+function draw () {
+    stroke(7,75,186);
+    strokeWeight(10);
+    fill(255,255,255,1);
+    ellipse(100,150,160,160);
+    
+    stroke(2,2,1);
+    strokeWeight(10);
+    fill(255,255,255,1);
+    ellipse(275,150,160,160);
+    
+    stroke(255, 5, 5);
+    strokeWeight(10);
+    fill(255,255,255,1);
+    ellipse(450,150,160,160);
+    
+    stroke(248, 255, 53);
+    strokeWeight(10);
+    fill(255,255,255,1);
+    ellipse(185,250,160,160);
+    
+    stroke(102, 255, 56);
+    strokeWeight(10);
+    fill(255,255,255,1);
+    ellipse(360,250,160,160);
+    
+}
